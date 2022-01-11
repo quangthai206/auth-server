c625d0b2dee0822ef1bee33792dc91ef02c401be
quangthai206 <kysubon@teneocto.io>
2022-01-11 17:25:24 +0700

    add log

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const Pusher = require("pusher");
 const pusher = new Pusher({
-  appId: "APP_ID",
-  key: "APP_KEY",
-  secret: "APP_SECRET",
-  cluster: "APP_CLUSTER",
+  appId: "1329740",
+  key: "35311733394757534659",
+  secret: "1400adb010651368c687",
+  cluster: "ap3",
   useTLS: true,
 });
 const app = express();
@@ -16,16 +16,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("What!")
+  res.send("Hello from test pusher server!")
 })
 app.post("/pusher/auth", (req, res) => {
   const socketId = req.body.socket_id;
   const channel = req.body.channel_name;
+  console.log(`new auth request: socketId = ${socketId}, channel = ${channel}`);
   const auth = pusher.authenticate(socketId, channel);
   res.send(auth);
 });
 
 const port = process.env.PORT || 1999;
 app.listen(port, () => {
-  console.log("Start listening on port 1999");
+  console.log(`Start listening on port ${port}`);
 });
