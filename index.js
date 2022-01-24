23e5a54cfa028c164fd8018544d716c9a97220be
quangthai206 <kysubon@teneocto.io>
2022-01-24 22:53:26 +0700

    add send gift endpoint

--- index.js
+++ index.js
@@ -86,6 +86,82 @@ app.post("/live/nice", (req, res) => {
     })
 });
 
+// SEND_GIFT
+app.post("/gift", (req, res) => {
+  let pusherBody = {
+    receiver: null,
+    type: "SEND_GIFT/1",
+    value: {
+      id: "2eb49b1c-355f-48dc-a175-47b94e39dfe9",
+      liveId: req.body.channelName,
+      localDateTime: "2022-01-24T12:06:34.577Z",
+      expiredDateTime: null,
+      sourceGiftType: {
+        id: "c87174c4-439f-4097-85b6-da78c218483d",
+        name: "HELLO!（軽）",
+        amount: 1,
+        coins: 1,
+        giftCategories: [
+          {
+            id: "cc471585-3a3c-4bff-b43d-5f8f7bac70c7",
+            name: "定番",
+            description: "まずはコレ！安心の定番ギフト"
+          }
+        ],
+        icon: {
+          id: "08528591-e4b7-4c25-a10c-1d84d6ec8cae",
+          uri: "https://user-cdn.dev.everylive.jp/582883969ff74feaa4029e420ecc62d4_2de46c3a954741b6971a8c1ca5605a3f",
+          mediaType: "image/png"
+        },
+        animation: {
+          id: "c56aa830-ab41-4602-ace2-46f75c152c91",
+          uri: "https://system-cdn.dev.everylive.jp/gift/animation/5812ec4a7bf143f39b652bea15f339f6_hello.png",
+          mediaType: "image/apng"
+        }
+      },
+      sender: req.body.user,
+      giftType: {
+        id: "c87174c4-439f-4097-85b6-da78c218483d",
+        name: "HELLO!（軽）",
+        amount: 1,
+        coins: 1,
+        giftCategories: [
+          {
+            id: "cc471585-3a3c-4bff-b43d-5f8f7bac70c7",
+            name: "定番",
+            description: "まずはコレ！安心の定番ギフト"
+          }
+        ],
+        icon: {
+          id: "08528591-e4b7-4c25-a10c-1d84d6ec8cae",
+          uri: "https://user-cdn.dev.everylive.jp/582883969ff74feaa4029e420ecc62d4_2de46c3a954741b6971a8c1ca5605a3f",
+          mediaType: "image/png"
+        },
+        animation: {
+          id: "c56aa830-ab41-4602-ace2-46f75c152c91",
+          uri: "https://system-cdn.dev.everylive.jp/gift/animation/5812ec4a7bf143f39b652bea15f339f6_hello.png",
+          mediaType: "image/apng"
+        }
+      },
+      combo: {
+        id: "7fa742c1-f1bb-4582-91be-3804ad6c79f3",
+        sendDateTime: "2022-01-24T12:06:35.340Z",
+        count: 1,
+        comboId: "7fa742c1-f1bb-4582-91be-3804ad6c79f3"
+      }
+    },
+    sendDateTime: "2022-01-24T12:06:35.414Z"
+  }
+
+  pusher.trigger(req.body.channelName, "message", pusherBody)
+    .then(() => {
+      res.json(pusherBody.value);
+    })
+    .catch(() => {
+      res.status(400).json({"message": "something wrong happened."});
+    })
+});
+
 const port = process.env.PORT || 1234;
 app.listen(port, () => {
   console.log(`Start listening on port ${port}`);
